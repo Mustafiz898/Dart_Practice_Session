ae61d55167016e9d299a9d1f6328372e37b9fce2
Mustafiz898 <mustafiz.ruet.898@gmail.com>
2025-11-28 17:55:41 +0600

    Practicing different operators (floor-dvision, is, is not, null-safety, null-coalescing, null-aware-assignment)

--- bin/operator.dart
+++ bin/operator.dart
@@ -7,12 +7,26 @@ void main(){
   print(result);
   print(result1);
 
-  // is operator checks type of variable at runtime
+  // is operator checks type of variable at runtime (also called type test operator)
   print(x is int);
   print(x is double);
 
   // is not operator (is!)
   print(x is! String);
+//--------------------------------------------------------------
+  // ?? null coalescing operator
+  String ? name;
+  print(name);
+  name = "SHUVO";
+  print(name ?? "Mustafiz");  // NULL Coalescing operator prints left side if the value is not null
+                              // Otherwise it prints right side value in this case Mustafiz
+//-----------------------------------------------------------------
+
+  // ??= null aware assignment
+  String ? location;
+  print(location ?? "Rajshahi");       // if the location is null prints Rajshahi
+  location ??= "Manikganj";            // if the location is null, then assign value Manikganj
+  print(location ?? "Dhaka");          // if the location is not null it prints location, otherwise prints Dhaka
 
 
 }
